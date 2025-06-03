efae3e532469ec28acb791168e861baefe7e1b3d
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-03 23:59:52 +0000

    going to testprimevue-

--- note/src/views/NotesView.vue
+++ note/src/views/NotesView.vue
@@ -7,7 +7,7 @@
                     class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
                 >
                     Refresh
-                </button> -->
+                </button> going to testprimevue-->
             <button
                 @click="logout"
                 class="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
