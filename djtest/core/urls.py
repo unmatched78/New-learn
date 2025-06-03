59b3d20b37c3d26ec140b196388af8ebed9029a2
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-03 20:22:11 +0000

    authentication

--- djtest/core/urls.py
+++ djtest/core/urls.py
@@ -1,13 +1,7 @@
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import (
-    MyTokenObtainPairView,
-    UserViewSet,
-    NoteViewSet,
-    RegisterView
-)
+from .views import *
 
 from rest_framework_simplejwt.views import TokenVerifyView
 from django.urls import path, include
@@ -19,9 +13,9 @@ router.register(r'users', UserViewSet)
 router.register(r'notes', NoteViewSet)
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),#for api users
+    path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),#for api users
     path('register/', RegisterView.as_view(), name='register'),
     path('', include(router.urls)),
 ]
