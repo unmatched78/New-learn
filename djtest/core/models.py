167489c4dc0e069d1e81f7984700e35cec909e9f
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-03 15:29:36 +0000

    nothing

--- djtest/core/models.py
+++ djtest/core/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import transaction
+from django.conf import settings
 
 # Create your models here.
--- djtest/djtest/urls.py
+++ djtest/djtest/urls.py
@@ -24,3 +24,14 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/', include('core.urls') )
 ]
+
+#serve satatic and media
+from django.conf.urls.static import static
+from django.conf import settings
+from settings import DEBUG
+# handle static files
+    
+if DEBUG:
+    # serve static files and media
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
