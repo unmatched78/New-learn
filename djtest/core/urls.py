9fc06c8b6b7af958cfb26a61649629e24515bdc5
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-03 14:45:10 +0000

    token verify

--- djtest/core/urls.py
+++ djtest/core/urls.py
@@ -2,10 +2,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from rest_framework_simplejwt.views import TokenVerifyView
 from django.urls import path
 urlpatterns = [
     ...
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),#for api users
     ...
 ]
