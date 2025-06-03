a48387fd39513a9780caf60c95b784a5a9f52b66
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-03 15:12:54 +0000

    nothing

--- djtest/djtest/urls.py
+++ djtest/djtest/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from settings.conf import static
+from django.contrib import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/', include('core.urls') )
 ]
