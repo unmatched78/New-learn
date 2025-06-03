871280639d2b04b7ac63ba1157fe405d9aecdd6f
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-03 21:52:04 +0000

    clean

--- note/vite.config.ts
+++ note/vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js',
+    },
+  },
 })
-
 // https://vite.dev/config/
