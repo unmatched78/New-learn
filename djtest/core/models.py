7c418c7e6d2e4b12b1786ce3150278c1f6b18f57
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-03 15:48:28 +0000

    added notes model

--- djtest/core/models.py
+++ djtest/core/models.py
@@ -4,3 +4,26 @@ from django.db import transaction
 from django.conf import settings
 
 # Create your models here.
+class Timer(models.Model):
+    created_at=models.DateTimeField(auto_add_now=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    class meta:
+        abstract=True
+
+class CustomUser(AbstractUser):
+    Roles=[
+        "boy":"boy",
+        "girl":"girl"
+    ]
+    role=models.CharField(choices="Roles", default="boy")
+    def __str__(self):
+        return f"{self.user.username} --{self.user.role}."
+
+class Note(Timer):
+    notewriter=models.Foreignkey(CustomUser, on_delete=models.set_null, related_name="notewriter")
+    note=models.TextField()
+    def __str__(self):
+        return f"{self.note[:20]} by {self.notewriter.username}"
+
+
+    
