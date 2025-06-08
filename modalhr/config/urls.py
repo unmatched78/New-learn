aedf1eaea0eea5996100d430aa77c9d406332735
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-08 14:38:55 +0000

    updated urls mina

--- modalhr/config/urls.py
+++ modalhr/config/urls.py
@@ -1,20 +1,24 @@
-### config/urls.py
+# config/urls.py
 
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
+from decouple import config
 
 # Core/public routes (public schema)
 urlpatterns = [
-    path(f'api/{settings.API_VERSION}/auth/', include('apps.accounts.urls')),
+    path(f'api/{settings.API_VERSION}/auth/',    include('apps.accounts.urls')),
     path(f'api/{settings.API_VERSION}/tenants/', include('apps.tenants.urls')),
     path(f'api/{settings.API_VERSION}/modules/', include('apps.module_manager.urls')),
 ]
 
-# Tenant-scoped module URLs
-for mod in config(
+# Dynamically include each HR module based on your ENABLED_MODULES setting
+ENABLED = config(
     'ENABLED_MODULES',
     default='employees,time_attendance,leave_management,payroll,performance,recruitment,benefits,learning'
-).split(','):
+).split(',')
+
+for mod in ENABLED:
     urlpatterns.append(
         path(
             f'api/{settings.API_VERSION}/{mod.replace("_", "-")}/',
@@ -22,7 +26,7 @@ for mod in config(
         )
     )
 
-# Static & media in DEBUG\ nfrom django.conf.urls.static import static
+# Serve static & media files in DEBUG
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
