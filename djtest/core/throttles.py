47c21df20b0a4414a079e8b4ca18a435012c4f60
Viella <168560797+unmatched78@users.noreply.github.com>
2025-06-08 12:51:42 +0000

    clean

--- djtest/core/throttles.py
+++
@@ -1,7 +0,0 @@
-# core/throttles.py
-
-from rest_framework.throttling import AnonRateThrottle
-
-class LoginRateThrottle(AnonRateThrottle):
-    scope = "login"
-    #rate = "10/minute"  # Allow 10 login attempts per minute for anonymous users
